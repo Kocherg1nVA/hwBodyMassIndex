cf04eb29534faca20243e44c46c479dbb5f088a5
Vadim Kochergin <kochergin1990vadim@gmail.com>
2024-11-14 11:38:48 +0300

    ParamChanged

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         BmiService service = new BmiService();
-        double height = 1.78;
-        double weight = 72.0;
+        double height = 1.87;
+        double weight = 98.0;
         int bmi = (int) service.calculate(height, weight);
         System.out.println("Ваш индекс массы тела: " + bmi);
     }
